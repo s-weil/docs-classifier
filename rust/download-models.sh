ed672eb4791bf75e1e83c3dece3fec38289901e1
steffen <steffen.weil@proton.me>
2024-09-22 07:21:31 +0200

    running version

---
+++ rust/download-models.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DETECTION_MODEL="https://ocrs-models.s3-accelerate.amazonaws.com/text-detection.rten"
+RECOGNITION_MODEL="https://ocrs-models.s3-accelerate.amazonaws.com/text-recognition.rten"
+
+mkdir models
+curl "$DETECTION_MODEL" -o ./models/text-detection.rten
+curl "$RECOGNITION_MODEL" -o ./models/text-recognition.rten
